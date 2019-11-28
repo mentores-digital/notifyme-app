780c83680d7076f93238898c15394212f0515454
Thiago Medeiros <thihxm@gmail.com>
2019-11-28 13:49:50 -0300

    login

---
+++ src/pages/Login.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import {
+  KeyboardAvoidingView,
+  View,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Switch,
+  StyleSheet,
+} from 'react-native';
+
+import CheckBox from '@react-native-community/checkbox';
+
+import logo from '../assets/logo.png';
+import background from '../assets/background.png';
+
+export default function Login() {
+  return (
+    <ImageBackground
+      source={background}
+      style={{ width: '100%', height: '100%' }}>
+      <KeyboardAvoidingView
+        behavior="padding"
+        enabled={Platform.OS === 'ios'}
+        style={styles.container}>
+        <Image style={styles.logo} source={logo} />
+
+        <Text style={styles.description}>Envie e receba notificações.</Text>
+
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Login"
+          placeholderTextColor="#fff"
+          style={styles.input}
+        />
+
+        <TextInput
+          autoCapitalize="none"
+          autoCorrect={false}
+          placeholder="Senha"
+          placeholderTextColor="#fff"
+          textContentType='password'
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+
+        <View style={styles.optionsContainer}>
+          <View style={styles.lembre}>
+            {Platform.OS === 'ios' && <Switch />}
+            {Platform.OS === 'android' && <CheckBox value={true} />}
+
+            <TouchableOpacity>
+              <Text style={styles.option}>Lembrar-me</Text>
+            </TouchableOpacity>
+          </View>
+
+          <TouchableOpacity>
+            <Text style={styles.option}>Esqueceu a senha?</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  logo: {
+    width: 158.2,
+    height: 40,
+  },
+
+  description: {
+    marginTop: 20,
+    marginBottom: 50,
+    fontSize: 20,
+    fontWeight: '300',
+    color: '#fff',
+    fontFamily: 'sans-serif',
+  },
+
+  input: {
+    height: 60,
+    alignSelf: 'stretch',
+    color: '#fff',
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    borderRadius: 7,
+    borderColor: 'rgba(255, 255, 255, 0.4)',
+    borderWidth: 1,
+    marginTop: 10,
+  },
+
+  lembre: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  button: {
+    height: 60,
+    alignSelf: 'stretch',
+    backgroundColor: '#0041c4',
+    borderRadius: 7,
+    marginTop: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+
+  optionsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 30
+  },
+
+  option: {
+    color: '#fff',
+  },
+});
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+import Login from './pages/Login';
+
+export default createAppContainer(
+  createSwitchNavigator({
+    Login,
+  })
+);
