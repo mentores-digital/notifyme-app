f6bb28b28ecc04eeee3de463ca3c93dd4abc01a2
Thiago Henrique Xavier Medeiros <thiago@mentores.com.br>
2019-11-29 10:32:47 -0300

    rota da pagina de cadastro

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import Cadastro from './pages/Cadastro';
 
 export default createAppContainer(
   createSwitchNavigator({
@@ -12,6 +13,22 @@ export default createAppContainer(
         screen: Dashboard,
         navigationOptions: ({navigation}) => ({
           title: 'Inicio',
+          // headerRight: (
+          //   <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
+          //     <Icon
+          //       style={{marginRight: 20}}
+          //       name="chat-processing"
+          //       size={24}
+          //       color="#000000"
+          //     />
+          //   </TouchableOpacity>
+          // ),
+        }),
+      },
+      Cadastrar: {
+        screen: Cadastro,
+        navigationOptions: ({navigation}) => ({
+          title: 'Cadastrar Usuário',
           // headerRight: (
           //   <TouchableOpacity onPress={() => navigation.navigate('Chat')}>
           //     <Icon
